6ed2401637528ffa55024b380d56265f3965adc2
Kenneth Crawford <kmc059000@gmail.com>
2012-07-08 01:28:09 -0500

    added common version file and set version to 0.0.1

--- src/trout.messagequeue/trout.messagequeue/Properties/AssemblyInfo.cs
+++ src/trout.messagequeue/trout.messagequeue/Properties/AssemblyInfo.cs
@@ -22,15 +22,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("989d7a0e-f56b-4f1c-bbae-211997df0961")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
